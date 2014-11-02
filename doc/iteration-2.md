9680a8e527792c4c91e305d460aaa47f9e7794a9
Alberto Ramirez <alberto@aramirez.es>
2014-11-02 13:52:09 +0100

    Refactor Ideas

--- doc/iteration-2.md
+++ doc/iteration-2.md
@@ -5,3 +5,5 @@ Para responder a esa pregunta, me creo un un método llamado "countVotesFor", qu
 La forma de almacenamiento no es más que otro array clave-valor, indexado por IdeaId y que contiene un array con los usuarios que han votado
 
 - Añado un par de testcases más para reforzar la "spec": ideas que no han sido votadas deben devolver cero. Cuando tratas de obtener número de votos de una idea inexistente, devuelve un errror.
+
+- Aplico algún refactoring necesario tanto en Ideas como en el test.
--- src/Kata/Ideas/Ideas.php
+++ src/Kata/Ideas/Ideas.php
@@ -9,30 +9,44 @@ class Ideas
 
     public function add(Idea $idea)
     {
-        $this->ideas[spl_object_hash($idea->getId())] = $idea;
-        $this->votes[spl_object_hash($idea->getId())] = [];
+        $this->ideas[$this->indexFor($idea->getId())] = $idea;
+        $this->votes[$this->indexFor($idea->getId())] = [];
     }
 
     public function get(IdeaId $idea_id)
     {
-        return
-            isset($this->ideas[spl_object_hash($idea_id)])
-                ? $this->ideas[spl_object_hash($idea_id)]
-                : null
+        return $this->ideaExists($idea_id)
+            ? $this->getIdea($idea_id)
+            : null
         ;
     }
 
     public function vote(IdeaId $idea_id, $user)
     {
-        $this->votes[spl_object_hash($idea_id)][] = $user;
+        $this->votes[$this->indexFor($idea_id)][] = $user;
     }
 
     public function countVotesFor(IdeaId $idea_id)
     {
-        if (!isset($this->votes[spl_object_hash($idea_id)])) {
+        if (!$this->ideaExists($idea_id)) {
             throw new \InvalidArgumentException("Idea with ID: $idea_id does not exist.");
         }
 
-        return count($this->votes[spl_object_hash($idea_id)]);
+        return count($this->votes[$this->indexFor($idea_id)]);
+    }
+
+    private function ideaExists(IdeaId $idea_id)
+    {
+        return isset($this->ideas[$this->indexFor($idea_id)]);
+    }
+
+    private function getIdea(IdeaId $idea_id)
+    {
+        return $this->ideas[$this->indexFor($idea_id)];
+    }
+
+    private function indexFor(IdeaId $idea_id)
+    {
+        return spl_object_hash($idea_id);
     }
 }
